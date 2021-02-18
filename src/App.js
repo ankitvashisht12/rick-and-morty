fd351af931d0d97e1aeaf2edd8b37cda3b0ab453
Ankit Vashisht <ankitvashisht12@gmail.com>
2021-02-18 20:24:03 +0530

    add search and bootstrap styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-   <>
-   </> 
+   <div className='container m-5 px-5'>
+     <h1 className='text-center py-5'>Rick and Morty Search</h1>
+     <input className='form-control text-center' type='text' value={searchTerm} placeholder='🔍 Search for a contact' onChange={e => setSearchTerm(e.target.value)}/>
+   </div> 
   );
 }
 

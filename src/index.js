f9960b695aae950cb10ce43564f7ac0631e88632
Ankit Vashisht <ankitvashisht12@gmail.com>
2021-02-19 00:26:20 +0530

    add bootstrap css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+// Setting base url
+axios.defaults.baseURL = 'https://rickandmortyapi.com/api/character/';
 
 ReactDOM.render(
   <React.StrictMode>

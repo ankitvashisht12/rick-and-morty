a8f468427f5066e5048d5cec0bab52a5bf5b74de
Ankit Vashisht <ankitvashisht12@gmail.com>
2021-02-20 12:28:19 +0530

    add infinite scrolling

--- src/components/Character/Chracter.js
+++ src/components/Character/Chracter.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import StatusIndicator from '../StatusIndicator/StatusIndicator';
 
-const Chracter = ({id, name, status, species, image}) => {
+const Chracter = ({id, refer, name, status, species, image}) => {
 	return (
-		<div className='card rounded m-3 d-flex flex-row justify-content-between align-items-center' style={{width: '100%'}}>
+		<div ref={refer} className='card rounded m-3 d-flex flex-row justify-content-between align-items-center' style={{width: '100%'}}>
 			<div className='d-flex flex-row align-items-center' >
 				<img src={image} alt='character' style={{width: '100px'}} className='rounded-circle p-3'/>
 				<h3 className='p-3 fs-light'>{name}</h3>	
--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import Character from '../Character/Chracter';
 
-const Characters = ({ characters, loading }) => {
+const Characters = ({ characters, loading, error, more, pageNo, setPageNo }) => {
+	
+	const observer = useRef();
 
-	if(loading) return <h1>Loading...</h1>
+	
+	const lastCharacter = useCallback(node => {
+			if(loading) return ;
+			if(observer.current) observer.current.disconnect();
+			
+			observer.current = new IntersectionObserver(entries => {
+
+				if(entries[0].isIntersecting && more){
+					console.log('calling setpage')
+					setPageNo(prevPageNo => prevPageNo + 1);
+				}
+
+			})
+
+			if(node) observer.current.observe(node);
+		},
+		[loading, more],
+	)
+
+	const characterList = characters.map((character, idx) => { 
+		if(characters.length === idx + 1)
+			return (
+				<Character key={character.id} 
+					refer={lastCharacter}
+					id={character.id}
+					name={character.name}
+					status={character.status}
+					species={character.species}
+					image={character.image}/>
+			);
+						
+	return (
+		<Character key={character.id} 
+			id={character.id}
+			name={character.name}
+			status={character.status}
+			species={character.species}
+			image={character.image}/>);
+	});
+
 
 	return (
 		<>
-			{ characters.map(character => {
-					return (
-						<Character key={characters.id} 
-						id={character.id}
-						name={character.name}
-						status={character.status}
-						species={character.species}
-						image={character.image}/>
-					);
-				})
-			} 
+			{characterList} 
 		</>
 		);
 	
